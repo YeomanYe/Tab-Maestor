a5cf7c7a8f8b0ee1189502d5136269a4bb5b7056
YeomanYe <ming13821007683@gmail.com>
2026-04-22 09:30:31 +0800

    feat: add trash icons to delete buttons and search functionality
    - Replace X icons with trash icons for tab and group delete buttons
    - Add search box component to header for filtering tabs
    - Add searchQuery and filteredTabs to TabStore
    - Add i18n support for search placeholder

--- src/popup/main.tsx
+++ src/popup/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@/contexts/ThemeContext';
 import Popup from './Popup';
 import '../styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Popup />
+    <ThemeProvider>
+      <Popup />
+    </ThemeProvider>
   </React.StrictMode>
 );
